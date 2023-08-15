4efcf72269424c284f0e92340f1e6d43e352afd8
mvdudin <mdudin73@gmail.com>
2023-08-15 23:33:47 +0400

    ДЗ 2

---
+++ HeapSort/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int array[] = {100, 18, 15, 9, 1, 8, 3, 101, 2, -7};
+        sort(array);
+        print(array);
+    }
+
+    public static void sort(int arr[]) {
+        for (int i = arr.length / 2 - 1; i >= 0; i--)
+            heapify(arr, arr.length, i);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(int arr[], int n, int i) {
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int l = 2 * i + 1; // левый = 2*i + 1
+        int r = 2 * i + 2; // правый = 2*i + 2
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
